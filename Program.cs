9f99228083e1bb29bf98acd6b7bd9ecb7a75840a
freeman888 <freemanwink@outlook.com>
2023-05-18 23:47:03 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+app.Urls.Add("http://*:10001");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
